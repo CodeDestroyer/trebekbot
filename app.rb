8e26bdaa4f045cd84741528c7b26a485303f70a3
Patrick Cunningham <pcunningham@patricks-mbp.xlm.corp>
2015-06-25 09:51:14 -0400

    updated help

--- app.rb
+++ app.rb
@@ -429,7 +429,7 @@ end
 #
 def respond_with_help
   reply = <<help
-Type `#{ENV["BOT_USERNAME"]} next q` to start a new round of Slack Jeopardy. I will pick the category and price. Anyone in the channel can respond.
+Type `#{ENV["BOT_USERNAME"]} new q` to start a new round of Slack Jeopardy. I will pick the category and price. Anyone in the channel can respond.
 Type `#{ENV["BOT_USERNAME"]} [what|where|who] [is|are] [answer]?` to respond to the active round. You have #{ENV["SECONDS_TO_ANSWER"]} seconds to answer. Remember, responses must be in the form of a question, e.g. `#{ENV["BOT_USERNAME"]} what is dirt?`.
 Type `#{ENV["BOT_USERNAME"]} what is my score` to see your current score.
 Type `#{ENV["BOT_USERNAME"]} show the leaderboard` to see the top scores.
